1b89ebc2a3bc6eb51ca21a771bcf71340ba5655e
gu <guymguym@gmail.com>
2014-04-20 04:53:59 +0300

    initial commit - env, deps, server

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+	// Default tasks
+	grunt.registerTask('default', [
+		'bower',
+		'jshint'
+		// 'concat',
+		// 'uglify'
+	]);
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bower: {
+			install: true
+		},
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'src/**/*.js'
+			]
+		},
+	});
+
+	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+
+	// Define custom tasks
+	grunt.task.registerMultiTask('bower', 'Bower', function() {
+		// Force task into async mode and grab a handle to the "done" function.
+		var done = this.async();
+		grunt.util.spawn({
+			cmd: './node_modules/.bin/bower',
+			args: [this.target]
+		}, done);
+	});
+
+};
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./node_modules/.bin/supervisor -w src -e 'js,html' src/server.js
